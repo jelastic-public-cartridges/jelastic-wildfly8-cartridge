646af041403966c6bde32732010452e7b06a064e
Dmytro Zubelevych <dz@jelastic.com>
2017-11-05 18:26:23 +0200

    added generate_optimal_configs.sh

--- jelastic/scripts/generate_optimal_configs.sh
+++ jelastic/scripts/generate_optimal_configs.sh
@@ -3,3 +3,7 @@
 echo -e "$(find $(realpath /usr/java/latest) -name libjli.so -printf "%h\n")" > /etc/ld.so.conf.d/java.conf ; \
 ldconfig
 
+JAVABIN=$(which java 2>/dev/null)
+[ ! -z "$JAVABIN" ] && {
+    setcap 'cap_net_bind_service=+ep' $(readlink -f "$JAVABIN")
+}
